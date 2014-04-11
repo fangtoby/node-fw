a5307ec0bfd120e7e3e8debea268c675f589219a
fang.yanliang <fang.yanliang@sh.adways.com>
2014-04-11 17:36:00 +0800

    add name/controllername

--- router.js
+++ router.js
@@ -15,7 +15,21 @@ function route(route, req, res) {
 		case 1:
 			break;
 		case 2:
+			var _controller = routes[1];
+			var _action = 'index';
+			
+			var currectController = requireFunc.include(_controller);
+			if (typeof currectController == 'object') {
+				console.log(typeof currectController[_action]);
+				if (typeof currectController[_action] == 'function') {
+					return currectController[_action](req, res);
+				} else {
+					console.log('No request handler found for ' + route);
+					return handle.error(res, 'No request handler found for ' + route);
+				}
+			} else {
 
+			}
 			break;
 		case 3:
 			var _controller = routes[1];
