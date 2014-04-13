9cc8b352986f222cc63043bca462bdd1e4df00ad
fang.yanliang <fang.yanliang@sh.adways.com>
2014-04-14 00:04:00 +0800

    modify create file

--- extends/makefile.js
+++ extends/makefile.js
@@ -5,40 +5,35 @@
  	if (controller.length > 2) {
  		var controllerName = controller[1];
  		var path = 'controller/' + controllerName + '.js';
- 		fs.exists(path, function(exist) {
- 			if (!exist) {
- 				fs.open(path, 'w', '0777');
- 				fs.close(path, function(success) {
+ 		if (!fs.existsSync(path)) {
+ 			var tempPath = 'controller/temple.js';
+ 			var content = fs.readFileSync(tempPath, 'utf-8');
+ 			fs.writeFileSync(path, content, 'utf-8');
+ 		}
 
- 				});
- 			}
- 		});
- 		var _basicView = ['view', '_view', '_item', 'menu', 'form', 'model','start', 'index','upload'];
+ 		var _basicView = ['view', '_view', '_item', 'menu', 'form', 'model', 'start', 'index', 'upload'];
  		var _viewPathSeq = 'view/';
  		var _suffix = '.html';
 
  		var _viewFieldPath = _viewPathSeq + controllerName;
+ 		var createView = false;
 
- 		fs.exists(_viewFieldPath, function(exist) {
- 			var createView = false;
- 			if (!exist) {
- 				fs.mkdir(_viewFieldPath, '0777', function(status) {
- 					if (status) {
- 						createView = true;
- 					}
- 				});
- 			} else {
+ 		if (fs.existsSync(_viewFieldPath)) {
+ 			createView = true;
+ 		} else {
+ 			if (fs.mkdirSync(_viewFieldPath, '0777')) {
  				createView = true;
  			}
- 			if (createView) {
- 				for (var i = 0; i < _basicView.length; i++) {
- 					var filePath = _viewFieldPath + '/' + _basicView[i] + _suffix;
- 					if (!fs.existsSync(filePath)) {
- 						fs.open(filePath, 'w', '0777');
- 					}
+ 		}
+ 		if (createView) {
+ 			for (var i = 0; i < _basicView.length; i++) {
+ 				var filePath = _viewFieldPath + '/' + _basicView[i] + _suffix;
+ 				if (!fs.existsSync(filePath)) {
+ 					var fd = fs.openSync(filePath, 'w', '0777');
+ 					fs.closeSync(fd);
  				}
  			}
- 		});
+ 		}
 
  	}
  }
