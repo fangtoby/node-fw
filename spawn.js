a6ef6402e81685bbe3227827946356ef3779b05a
honest lies <fangtoby@live.cn>
2014-04-21 08:10:25 +0800

    add node modules

--- spawn.js
+++ spawn.js
@@ -3,26 +3,26 @@ var spawn = require('child_process').spawn,
 
 
 function startServer() {
-    console.log('start server');
-    server = spawn('../node_modules/nodemon/bin/nodemon.js', ['app.js']);
-    console.log('node js pid is ' + server.pid);
-    server.on('close', function(code, signal) {
-        server.kill(signal);
-        server = startServer();
-    });
-    server.on('error', function(code, signal) {
-        server.kill(signal);
-        server = startServer();
-    });
-    server.stderr.on('data', function(argument) {
-        // body...
-        console.log(argument.toString());
-    });
-    server.stdout.on('data', function(argument) {
-        // body...
-        console.log(argument.toString());
-    });
-    return server;
+	console.log('start server');
+	server = spawn('../node_modules/nodemon/bin/nodemon.js', ['app.js']);
+	console.log('node js pid is ' + server.pid);
+	server.on('close', function(code, signal) {
+			server.kill(signal);
+			server = startServer();
+			});
+	server.on('error', function(code, signal) {
+			server.kill(signal);
+			server = startServer();
+			});
+	server.stderr.on('data', function(argument) {
+			// body...
+			console.log(argument.toString());
+			});
+	server.stdout.on('data', function(argument) {
+			// body...
+			console.log(argument.toString());
+			});
+	return server;
 };
 
 startServer();
