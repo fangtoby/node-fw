a69c34da76b23d256c86007afe0ee2ea019d88b1
honest lies <fangtoby@live.cn>
2014-04-22 19:44:07 +0800

    update km.sql file

--- data/km.sql
+++ data/km.sql
@@ -18,6 +18,11 @@
 --
 -- Table structure for table `authority`
 --
+DROP DATABASE if EXISTS `km`;
+
+CREATE DATABASE km;
+
+USE km;
 
 DROP TABLE IF EXISTS `authority`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
